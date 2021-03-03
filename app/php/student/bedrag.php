6702df10f04e5bf33baeab03dc2498bb0a750824
Ella716 <60780983+Ella716@users.noreply.github.com>
2021-03-03 12:48:36 -0600

    Add files via upload

--- app/php/student/bedrag.php
+++ app/php/student/bedrag.php
@@ -5,12 +5,11 @@ include "../conn.php";
 $stud_ID=$_SESSION['stud_ID'];
 $dt=date("Y-m-d");
 $bedrag=$_POST['bedrag'];
-$sql = "INSERT INTO `upreq` (`ID`, `Student_ID`, `Bedrag`, `Datum`, `Status`) VALUES (NULL, '$stud_ID', '$bedrag', '$dt', 'False')";
+$sql = "INSERT INTO upreq (ID, Student_ID, Bedrag, Datum, Status) VALUES (NULL, '$stud_ID', '$bedrag', '$dt', 'False')";
 if (mysqli_query($conn, $sql)) {
-  echo json_encode(array("statusCode"=>200));
-}
+echo "data inserted";}
 else {
-  echo json_encode(array("statusCode"=>201));
+echo "failed";
 }
 mysqli_close($conn);
 
