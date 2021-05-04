1c17ffb4597aa5f4b0de02d7ada28feb8ea4b995
Ella716 <60780983+Ella716@users.noreply.github.com>
2021-05-03 19:20:35 -0500

    Add files via upload

--- pages/admin/klassen-formatie.php
+++ pages/admin/klassen-formatie.php
@@ -1,3 +1,7 @@
+<?php
+include_once("../../app/php/conn.php");
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -30,167 +34,87 @@
 						<div class="card-header border-0">
 							<div class="row">
 								<div class="col-6">
-									<h3 class="mb-0">Striped table</h3>
-								</div>
-								<div class="col-6 text-right">
-									<a href="#" class="btn btn-sm btn-primary btn-round btn-icon" data-toggle="tooltip" data-original-title="Edit product">
-										<span class="btn-inner--icon"><i class="fas fa-user-edit"></i></span>
-										<span class="btn-inner--text">Export</span>
-									</a>
+									<h3 class="mb-0">Klassen-Formatie</h3>
 								</div>
 							</div>
 						</div>
 						<!-- Light table -->
 						<div class="table-responsive">
-						<table class="table align-items-center table-flush table-striped">
+							<table class="table align-items-center table-flush table-striped">
 								<thead class="thead-light">
 									<tr>
-										<th>Author</th>
-										<th>Created at</th>
-										<th>Product</th>
-										<th></th>
+										<th>Student achternaam</th>
+										<th>Student voornaam</th>
+										<th>email adress</th>
+										<th>Klasnaam</th>
+										<th>Richtingnaam</th>
+										<th>Klasdocent</th>
+										<th>Schooljaar</th>
+										<th>Actie</th>
 									</tr>
 								</thead>
-								<tbody>
-									<tr>
-										<td class="table-user">
-											<b>John Michael</b>
-										</td>
-										<td>
-											<span class="text-muted">10/09/2018</span>
-										</td>
-										<td>
-											<a href="#!" class="font-weight-bold">DATA</a>
-										</td>
-										<td class="table-actions">
-											<a href="#!" class="table-action" data-toggle="tooltip" data-original-title="Edit product">
-												<i class="fas fa-user-edit"></i>
-											</a>
-											<a href="#!" class="table-action table-action-delete" data-toggle="tooltip" data-original-title="Delete product">
-												<i class="fas fa-trash"></i>
-											</a>
-										</td>
-									</tr>
-									<tr>
-										<td class="table-user">
-											<b>Alex Smith</b>
-										</td>
-										<td>
-											<span class="text-muted">08/09/2018</span>
-										</td>
-										<td>
-											<a href="#!" class="font-weight-bold">DATA</a>
-										</td>
-										<td class="table-actions">
-											<a href="#!" class="table-action" data-toggle="tooltip" data-original-title="Edit product">
-												<i class="fas fa-user-edit"></i>
-											</a>
-											<a href="#!" class="table-action table-action-delete" data-toggle="tooltip" data-original-title="Delete product">
-												<i class="fas fa-trash"></i>
-											</a>
-										</td>
-									</tr>
-									<tr>
-										<td class="table-user">
-											<b>Samantha Ivy</b>
-										</td>
-										<td>
-											<span class="text-muted">30/08/2018</span>
-										</td>
-										<td>
-											<a href="#!" class="font-weight-bold">DATA</a>
-										</td>
-										<td class="table-actions">
-											<a href="#!" class="table-action" data-toggle="tooltip" data-original-title="Edit product">
-												<i class="fas fa-user-edit"></i>
-											</a>
-											<a href="#!" class="table-action table-action-delete" data-toggle="tooltip" data-original-title="Delete product">
-												<i class="fas fa-trash"></i>
-											</a>
-										</td>
-									</tr>
-									<tr>
-										<td class="table-user">
-											<b>John Michael</b>
-										</td>
-										<td>
-											<span class="text-muted">10/09/2018</span>
-										</td>
-										<td>
-											<a href="#!" class="font-weight-bold">DATA</a>
-										</td>
-										<td class="table-actions">
-											<a href="#!" class="table-action" data-toggle="tooltip" data-original-title="Edit product">
-												<i class="fas fa-user-edit"></i>
-											</a>
-											<a href="#!" class="table-action table-action-delete" data-toggle="tooltip" data-original-title="Delete product">
-												<i class="fas fa-trash"></i>
-											</a>
-										</td>
-									</tr>
-									<tr>
-										<td class="table-user">
-											<b>John Michael</b>
-										</td>
-										<td>
-											<span class="text-muted">10/09/2018</span>
-										</td>
-										<td>
-											<a href="#!" class="font-weight-bold">DATA</a>
-										</td>
-										<td class="table-actions">
-											<a href="#!" class="table-action" data-toggle="tooltip" data-original-title="Edit product">
-												<i class="fas fa-user-edit"></i>
-											</a>
-											<a href="#!" class="table-action table-action-delete" data-toggle="tooltip" data-original-title="Delete product">
-												<i class="fas fa-trash"></i>
-											</a>
-										</td>
-									</tr>
+								<tbody id="table">
+
 								</tbody>
 							</table>
-						</div>
+						</div>					
 					</div>
 					<button type="button" class="fab" data-toggle="modal" data-target="#modal"><i class="ni ni-fat-add ni-2x"></i></button>
 
-					<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
+					<div class="modal fade " id="modal" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
 						<div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
 							<div class="modal-content">
 								<div class="modal-header">
-									<h6 class="modal-title" id="modal-title-default">Document Registratie</h6>
+									<h6 class="modal-title" id="modal-title-default">Docent Registratie</h6>
 									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 										<span aria-hidden="true">×</span>
 									</button>
 								</div>
-								<div class="modal-body">
-									<form action="" id="districten-form">
+								<div  id="modal-body" class="modal-body">
+								<form method="post" id="KlassefForm">
 										<div class="row">
-											<div class="col-md-6">
+											<div class="col-md-12">
 												<div class="form-group">
-													<label for="">INPUT</label>
+													<label for="">Naam:</label>
 													<div class="input-group input-group-merge">
 														<div class="input-group-prepend">
-															<span class="input-group-text"><i class="fas fa-map-marker"></i></span>
+															<span class="input-group-text"><i class="fas fa-user"></i></span>
 														</div>
-														<input class="form-control" placeholder="Input 1" id="#" type="text">
+														<select class="form-control" name="Naam" id="Naam" required> 
+															<option>Selecteer Een Student</option>
+															<?php
+															$query = "SELECT * FROM studenten";
+															$result = mysqli_query($conn, $query);
+															while ($row = mysqli_fetch_assoc($result)) {
+																echo "<option value='" . $row['stud_ID'] . "'>" . $row['Achternaam'] . " " . $row['Voornaam'] . "</option>";
+															}
+															?>
+														</select>													
 													</div>
 												</div>
 											</div>
-											<div class="col-md-6">
+											<div class="col-md-12">
 												<div class="form-group">
-													<label for="">INPUT</label>
+													<label for="">Klas:</label>
 													<div class="input-group input-group-merge">
 														<div class="input-group-prepend">
-															<span class="input-group-text"><i class="fas fa-map-marker"></i></span>
+															<span class="input-group-text"><i class="fas fa-envelope-open-text"></i></span>
 														</div>
-														<input class="form-control" placeholder="Input 1" id="#" type="text">
+														<select class="form-control" name="Klas" id="Klas">
+															<option>Selecteer Een Klas</option>
+															<?php
+															$query = "SELECT * FROM klassen";
+															$result = mysqli_query($conn, $query);
+															while ($row = mysqli_fetch_assoc($result)) {
+																echo "<option value='" . $row['ID'] . "'>" . $row['Klas'] . "</option>";
+															}
+															?>
+														</select>
 													</div>
 												</div>
 											</div>
-										</div>
-
 										<div class="modal-footer">
-											<button type="button" class="btn btn-primary">Toevoegen</button>
+										<input type="submit" name="form_action" id="form_action" class="btn btn-primary" value="Toevoegen" />
 											<button type="button" class="btn btn-danger  ml-auto" data-dismiss="modal">Sluiten</button>
 										</div>
 									</form>
@@ -198,10 +122,20 @@
 							</div>
 						</div>
 					</div>
+					
 					<!-- Footer -->
 					<?php
 					include "../../includes/admin/footer.php"
 					?>
+					<script src="../../app/php/admin/script/crudKlasform.js"></script>
+					<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+					<script>
+						$(document).ready(function() {
+							load_data();
+
+						});
+					</script>
+
 
 </body>
 
