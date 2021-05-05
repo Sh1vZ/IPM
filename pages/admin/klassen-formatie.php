5fa9b5a2ae4a68d99218cc9f8f953ee0063f667a
Ginavira <viroja.kartodikromo.natin@gmail.com>
2021-05-05 15:37:54 -0300

    kleine taalfout i guess

--- pages/admin/klassen-formatie.php
+++ pages/admin/klassen-formatie.php
@@ -65,7 +65,7 @@ include_once("../../app/php/conn.php");
 						<div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
 							<div class="modal-content">
 								<div class="modal-header">
-									<h6 class="modal-title" id="modal-title-default">Docent Registratie</h6>
+									<h6 class="modal-title" id="modal-title-default">Klassenformatie Registratie</h6>
 									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 										<span aria-hidden="true">×</span>
 									</button>
